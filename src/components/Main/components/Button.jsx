0b85326ab3415500b91aa7ae6a68aa9f9914fd7e
AgustinGonzalez <agustin_.gonzalez_@hotmail.com>
2023-07-01 12:41:18 -0300

    font

--- src/components/Main/components/Button.jsx
+++ src/components/Main/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ function1, text1 }) => {
 	return (
 		<button
-			className="p-2 border-2 w-full relative bg-transparent z-[2] button group transition-all duration-300 ease-in-out border-oscuro1 hover:text-white md:text-lg text-xs"
+			className="p-2 border-2 w-full relative bg-transparent z-[2] button group transition-all duration-300 ease-in-out border-oscuro1 hover:text-white md:text-sm text-xs"
 			onClick={function1}>
 			<span className="absolute bg-oscuro1 h-full w-0 top-0 left-0 z-[-1] button-span1 group-hover:w-1/2 duration-300 ease-in-out"></span>
 			{text1}
